3c2d5cf14c6f51af1a30671adf679e48b5624f8c
Romal George <romalgeorge3@gmail.com>
2020-10-14 18:41:46 -0400

    Added activity

---
+++ RomalLab2/app/src/main/java/romal/george/s301107788/georgeActivityDetachedHome.java
@@ -0,0 +1,14 @@
+package romal.george.s301107788;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class georgeActivityDetachedHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_george_detached_home);
+    }
+}
